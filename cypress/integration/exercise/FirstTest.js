95ee1cd9678276187a9cd41a7003147c12a611a8
ChrAdrian <chr.adrian@yahoo.com>
2021-12-06 21:14:06 +0200

    + locators

--- cypress/integration/exercise/FirstTest.js
+++ cypress/integration/exercise/FirstTest.js
@@ -1,10 +1,10 @@
 describe('Test Suite', function() {
-    it('Verify Title of the Page', function() {
+    it('Verify Title of the Page - Positive', function() {
         cy.visit('https://www.emag.ro/')
         cy.title().should('eq','eMAG.ro - Libertate în fiecare zi')
     })
 
-    it('Verify Title of the Page', function() {
+    it('Verify Title of the Page - Negative', function() {
         cy.visit('https://www.emag.ro/')
         cy.title().should('eq','eMAG.ro')
     })
---
+++ cypress/integration/exercise/Locators.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />       // for autosuggestions
+
+describe("Locating elements", function(){
+    it("Verify types of locators", function()
+    {
+        cy.visit("https://www.emag.ro/")     // open the URL
+
+        cy.get("#searchboxTrigger").type("Playstation 5")       // type a product name in the search field
+
+        cy.get("#masthead > div > div > div.navbar-searchbox > div > form > div.input-group.searchbox-input > div.input-group-btn > button.btn.btn-default.searchbox-submit-button > i").click()      // click on search button
+
+        cy.get('.yeahIWantThisProduct[data-offer-id="77531692"]').click()
+
+        cy.get('.btn-block[data-dismiss="modal"]').click()
+
+        cy.get("#vendorsContainer > div > div.cart-widget.cart-line > div > div.main-product-details-container.emg-right > div.line-item.line-item-main > div.line-price-container.emg-right > div > p.product-new-price > span.money-int").contains("5.189")      // validation point
+
+        cy.get("#vendorsContainer > div > div.cart-widget.cart-line > div > div.main-product-details-container.emg-right > div.line-item.line-item-main > div.line-price-container.emg-right > div > p.product-new-price > sup").contains("99")    
+    })
+})
