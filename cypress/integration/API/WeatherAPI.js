b52757ad9578f4c5f5ee9702eac1204ebc3187cc
ChrAdrian <chr.adrian@yahoo.com>
2021-12-13 11:24:04 +0200

    + linked GET

---
+++ cypress/integration/API/WeatherAPI.js
@@ -0,0 +1,47 @@
+/// <reference types="cypress" />
+
+describe("Check weather information", ()=>{
+    it('Get weather informations for cities', ()=>{
+
+        cy.request({
+            method: 'GET',
+            url: 'https://www.metaweather.com/api/location/search/?query=Buc'
+        
+        }).then((res)=>{
+           const city = res.body[0].title
+           return city
+        
+        }).then((city)=>{
+            
+            cy.request({
+                method: 'GET',
+                url: 'https://www.metaweather.com/api/location/search/?query='+city
+            
+            }).then((res)=>{
+                expect(res.status).to.eq(200)
+                expect(res.body[0]).to.have.property('title', city)
+            })
+        })
+    })
+
+    it.only('Get weather informations for cities', ()=>{
+
+        cy.request({
+            method: 'GET',
+            url: 'https://www.metaweather.com/api/location/search/?query=Am'
+
+        }).then((resp)=>{
+            const len = resp.body.lenght
+            for(let i=0; i<13; i++){
+                cy.request({
+                    method: 'GET',
+                    url: 'https://www.metaweather.com/api/location/search/?query='+resp.body[i].title
+
+                }).then((res)=>{
+                    expect(res.status).to.eq(200)
+                    expect(res.body[0]).to.have.property('title', resp.body[i].title)
+            })
+        }
+        })
+    })
+})
